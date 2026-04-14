98553bfb762eb354ac35cef542c79d0db7ae8798
Camila Lucero <camilalucero076@gmail.com>
2026-04-14 00:51:44 -0300

    Creación de la clase excepción EstudianteNoEncontrado

---
+++ biblioteca1/src/main/java/unlar/edu/ar/Excepciones/EstudianteNoEncontradoException.java
@@ -0,0 +1,9 @@
+package unlar.edu.ar.Excepciones;
+
+public class EstudianteNoEncontradoException extends Exception {
+
+    public EstudianteNoEncontradoException(String message) {
+        super(message);
+    }
+
+}
