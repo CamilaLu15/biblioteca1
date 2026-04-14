ddbc44c0ebba937aad682de65a62809f1dccaa14
Camila Lucero <camilalucero076@gmail.com>
2026-04-14 00:54:41 -0300

    Creación de la clase excepción LimitePrestamosExcedido

---
+++ biblioteca1/src/main/java/unlar/edu/ar/Excepciones/LimitePrestamosExcedidoException.java
@@ -0,0 +1,9 @@
+package unlar.edu.ar.Excepciones;
+
+public class LimitePrestamosExcedidoException extends Exception {
+
+    public LimitePrestamosExcedidoException(String message) {
+        super(message);
+    }
+
+}
